5ee4e1a0fb44e5d3d0846543647b7b0c5b11ac3f
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-10-26 19:24:32 -0400

    styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-cv';
+  name = 'Robert W. Mahoney';
+  address = '7 King Arthur Ct. Fairfield, OH 45014';
+  github = 'https://github.com/rmaho31';
+  email = 'robert.w.mahoney@gmail.com';
+  image = "images/20180818_161518.jpg";
+  
+
 }
