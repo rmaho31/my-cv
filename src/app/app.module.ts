488c4f295b66e411c6fdbd3b3e61e8677e3f0a0a
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-10-26 19:26:27 -0400

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObjectiveComponent } from './features/objective/objective.component';
+import { TechnicalComponent } from './features/technical/technical.component';
+import { ProfessionalComponent } from './features/professional/professional.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObjectiveComponent,
+    TechnicalComponent,
+    ProfessionalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [ObjectiveComponent, TechnicalComponent, ProfessionalComponent]
 })
 export class AppModule { }
